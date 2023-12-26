ddc5ad252b0fa03563954343545481a20a0f8dc7
Abinaya642 <154853941+Abinaya642@users.noreply.github.com>
2023-12-26 08:32:47 +0530

    Add files via upload

---
+++ includes/myaccount-sidebar.php
@@ -0,0 +1,28 @@
+	<div class="col-md-4">
+					<!-- checkout-progress-sidebar -->
+<div class="checkout-progress-sidebar ">
+	<div class="panel-group">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+		    	<h3 class="unicase-checkout-title"><i>Your Checkout Progress</i></h3>
+		    </div>
+		    <div class="panel-body">
+				<ul class="nav nav-checkout-progress list-unstyled">
+					<li><a href="my-account.php"><i>My Account</i></a></li>
+					<li><a href="bill-ship-addresses.php"><i>Shipping / Billing Address</i></a></li>
+					<li><a href="order-history.php"><i>Order History</i></a></li>
+					<li><a href="pending-orders.php"><i>Payment Pending Orders</i>r</a></li>
+				</ul>		
+			</div>
+		</div>
+	</div>
+</div> 
+<!-- checkout-progress-sidebar -->				</div>
+
+
+
+
+
+
+
+
